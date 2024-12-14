d7e4f048e1bbe1a1fe6204538591e6e49988fd01
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2024-12-14 18:07:33 +0900

    탑&사이드

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,13 @@
-const Sidebar = () => {
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+
+const SideBar = () => {
   return <div>Sidebar</div>;
 };
 
-export default Sidebar;
+export default SideBar;
